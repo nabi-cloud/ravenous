baec9c1a727bb7de5e8b7cb8c1dcb69548d0b7fb
Nabi <matibag.abi@gmail.com>
2023-12-15 23:36:32 +0800

    Added link to CORS for users to follow

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -9,7 +9,7 @@ const BusinessList = ({ businesses }) => {
             {/* Get each data of business in array */}
             {
                 businesses.length === 0 ?
-                <h2 className={ styles.initalText }>Try searching for a restaurant near you</h2> 
+                <h2 className={ styles.initalText }>You may need to visit this <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank" rel="noopener noreferrer">link</a> first and request temporary access in order to display search results</h2> 
                 : businesses.map((business) => {
                     return <Business business={business} key={business.name} />;
                 })
